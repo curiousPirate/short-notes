353e66e680cbd495a891fe9e230f891a09fb0e22
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-01-07 15:53:07 +0530

    added code in main.js

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,31 @@
-// var topic = "HTML";
-
-// if (topic === 'HTML') {
-//   console.log("Let's study HTML!");
-// } else if (topic === 'CSS') {
-//   console.log("Let's study CSS!");
-// } else if (topic === 'Git') {
-//   console.log("Let's study Git!");
-// } else if (topic === 'JavaScript') {
-//   console.log("Let's study JavaScript!");
-// } else {
-//   console.log('Please try again!');
-// }
-
 var topics = ["HTML", "CSS", "GIT", "JS"];
 
-for (x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+var randomTopics = topics[Math.floor(Math.random() * topics.length)];
+
+function studytopics() {
+    
+    for (x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+    
 }
+
+function selecttopic() {
+ 
+    if (randomTopics === 'HTML') {
+        console.log("Let's study HTML!");
+      } else if (randomTopics === 'CSS') {
+        console.log("Let's study CSS!");
+      } else if (randomTopics === 'Git') {
+        console.log("Let's study Git!");
+      } else if (randomTopics === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+      } else {
+        console.log('Please try again!');
+      }
+}
+
+console.log('Here are the topics we learned through Prework:');
+studytopics();
+console.log('Which topic should we study first?');
+selecttopic();
